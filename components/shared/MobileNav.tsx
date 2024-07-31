714eb075440b77ea6820746fc82320b39dae0c2a
sambit Behera <bsambit82@gmail.com>
2024-08-01 00:02:38 +0530

    payment integration

--- components/shared/MobileNav.tsx
+++ components/shared/MobileNav.tsx
@@ -24,7 +24,7 @@ const MobileNav = () => {
 
       <nav className="flex gap-2">
         <SignedIn>
-          <UserButton afterSignOutUrl="/" />
+          <UserButton afterSwitchSessionUrl="/" />
 
           <Sheet>
             <SheetTrigger>
