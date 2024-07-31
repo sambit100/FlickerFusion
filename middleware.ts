377417de81d00336e777fdcaed69bf330a2f9282
sambit Behera <bsambit82@gmail.com>
2024-08-01 00:32:05 +0530

    public url added

--- middleware.ts
+++ middleware.ts
@@ -1,6 +1,17 @@
-import { clerkMiddleware } from "@clerk/nextjs/server";
+import {
+  clerkMiddleware,
+  createRouteMatcher
+} from '@clerk/nextjs/server';
 
-export default clerkMiddleware({
+const isProtectedRoute = createRouteMatcher([
+  '/',
+  '/api/webhooks/clerk',
+  '/api/webhooks/stripe'
+
+]);
+
+export default clerkMiddleware((auth, req) => {
+  if (isProtectedRoute(req)) auth().protect();
 });
 
 export const config = {
